c350574936acc0be670301762da9b9f210efaec0
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2023-02-09 17:26:32 -0400

    2nd: Setup form & deploy

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,7 @@ const Form = () => {
       <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
         <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-yellow-400 dark:text-white">Contáctame</h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-        <form action="https://formspree.io/f/mrgdvpwd" method="POST" class="space-y-8">
+        <form action="https://formsubmit.co/thesmartproject3.0@gmail.com" method="POST" class="space-y-8">
           <div>
             <label for="name" class="block mb-2 text-sm font-medium text-white dark:text-gray-300">Nombre</label>
             <input name="name" type="text" id="name" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="example@email.com" required />
